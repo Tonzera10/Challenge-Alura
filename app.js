5d4da29e78fafd5fc62e975976150442da71c0d4
Everton Mello <everton.mello.281@gmail.com>
2024-02-16 00:25:07 -0300

    falta pouco

--- app.js
+++ app.js
@@ -1,64 +1,48 @@
-let texto = document.querySelector("input");
-let resposta = document.querySelector("textarea");
+let texto = document.getElementById("inputText");
 let output = document.getElementById("output");
-let imagem = document.getElementById("imagem-output");
-let txtOutput = document.getElementById("texto-output");
-let botaoCopiar = document.getElementById("botao-copiar");
+let outputTexto = document.getElementById('output-texto');
 
-function converter() {
-  if (texto.value == "") {
-    alert("Por favor, digite  algo para ser convertido!");
-  } else {
-    output.style.display = "flex";
-    output.style.flexDirection = "column";
-    output.style.justifyContent = "space-beetween";
-    resposta.style.display = "block";
-    imagem.style.display = "none";
-    txtOutput.style.display = "none";
-    botaoCopiar.style.display = "block";
+function apenasMinusculas(event) {
+  const charCode = event.which || event.keyCode;
+  const charStr = String.fromCharCode(charCode);
+  if (/[^a-z]/.test(charStr)) {
+    event.preventDefault();
+    return false;
   }
+  return true;
 }
 
 function criptografar() {
-  console.log("cliquei");
-  // switch (texto.value) {
-  //     case texto.value.includes('e'):
-  //         texto.replace("e","enter");
-  //     case texto.value.includes('i'):
-  //         texto.replace("i","imes");
-  //     case texto.value.includes('a'):
-  //         texto.replace("a","ai");
-  //     case texto.value.includes('o'):
-  //         texto.replace("o","ober");
-  //     case texto.value.includes('u'):
-  //         texto.replace("u","ufat");
-  //     default:
-  //         break;
-  // };
-  // resposta = texto.value;
-  // converter();
+  const textoCriptografado = criptografarTexto(texto.value);
+  outputTexto.innerText = textoCriptografado;
+}
+
+function criptografarTexto(texto) {
+  return texto
+    .replace(/e/g, 'enter')
+    .replace(/i/g, 'imes')
+    .replace(/a/g, 'ai')
+    .replace(/o/g, 'ober')
+    .replace(/u/g, 'ufat');
 }
 
 function descriptografar() {
-  switch (texto.value) {
-    case texto.value.includes("enter"):
-      texto.replace("enter", "e");
-    case texto.value.includes("imes"):
-      texto.replace("imes", "i");
-    case texto.value.includes("ai"):
-      texto.replace("ai", "a");
-    case texto.value.includes("ober"):
-      texto.replace("ober", "o");
-    case texto.value.includes("ufat"):
-      texto.replace("ufat", "u");
-    default:
-      break;
-  }
-  resposta = texto.value;
-  converter();
+  const textoDescriptografado = descriptografarTexto(texto.value);
+  outputTexto.innerText = textoDescriptografado;
+}
+
+function descriptografarTexto(texto) {
+  return texto
+    .replace(/enter/g, 'e')
+    .replace(/imes/g, 'i')
+    .replace(/ai/g, 'a')
+    .replace(/ober/g, 'o')
+    .replace(/ufat/g, 'u');
 }
 
 function copiar() {
   window.navigator.clipboard.writeText(resposta.value);
   alert("Copiado com sucesso!");
 }
+
+
